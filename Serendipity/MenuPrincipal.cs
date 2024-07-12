7881ef5d76c5d54d97fd3629ef0ab3cb4d8d5467
Leonil265 <133721821+Leonil265@users.noreply.github.com>
2024-07-11 21:31:48 -0400

    Add files via upload

---
+++ Serendipity/MenuPrincipal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Serendipity
+{
+    public partial class menuPrincipal : Form
+    {
+        public menuPrincipal()
+        {
+            InitializeComponent();
+            this.Width = Screen.PrimaryScreen.Bounds.Width;
+            this.Height = Screen.PrimaryScreen.Bounds.Height;
+            this.Location = new Point(0, 0);
+        }
+    }
+}
